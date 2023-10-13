cd306c05bf63195ca7af64fb3a0f14b2f4b3c33a
Azure_Moon <jordanw0412@gmail.com>
2023-10-12 22:36:00 -0400

    fixed bugs

--- src/pages/Contact_Us.jsx
+++ src/pages/Contact_Us.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { useEffect } from 'react'
 
 const Contact_Us = () => {
-  useEffect(()=>{
-    console.log("hiyo")
-  },[])
+  
+  const handleSubmit = () =>{
+    console.log(formData)
+  }
+
   return (
     <>
     <div className='form-container'>
-        <form className='contact-form'>
-            First Name:<input type="text" name ="name"></input>
-            Email: <input type="email" name="Email"></input>
+        <form onSubmit={() =>{handleSubmit(email)}} className='contact-form'>
+            First Name:<input type="text" name ="first_name" required></input>
+            Email: <input type="email" name="email" required></input>
             Message:<input className='message-box' type='text' required></input>
-            <button type='submit'>Submit</button>
+            <input type="submit" value="Submit"></input>
         </form>
     </div>
     </>
